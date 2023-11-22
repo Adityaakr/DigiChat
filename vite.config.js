677e3b1533224d2881b6f493f2383fc507e76cc0
Akumar <128833380+Adityaakr@users.noreply.github.com>
2023-11-22 18:50:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        // Add any other external modules if needed
+      ],
+      output: {
+        assetFileNames: 'images/[name].[ext]',
+      },
+    },
+  },
+});
