f46e7a80bcb394cfad2952fb5956c8cb9e14d53b
Akumar <128833380+Adityaakr@users.noreply.github.com>
2023-11-23 17:53:11 +0530

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import add from "/src/img/add.png"
+import Video from "/src/img/pic.png"
 
 function Message(){
         return(
            <div className="message owner">
                 <div className="messageInfo">
-                        <img src= "add.png"/>
+                        <img src= {add}/>
                         <span> Just Now</span>
                 </div>
                 <div className="messageContent">
-                 <img src= "Video.png" alt="" />
+                 <img src= {Video} alt="" />
 
                  <p>hello</p>
                  </div>
