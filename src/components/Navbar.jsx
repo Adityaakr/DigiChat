2a958858855b5ae1c251ac07d587b9244af423b1
Akumar <128833380+Adityaakr@users.noreply.github.com>
2024-01-02 14:01:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { AuthContext } from "../Context/AuthContext";
 
 function Navbar() {
+
+        //getting reatime image that user selected-image to show
+        const {currentUser} = useContext(AuthContext)
         return(
                 <div className="navbar">
                         <span className="logo">DigiChat</span>
                         <div className="user">
-                          <img src="https://images.pexels.com/photos/3094799/pexels-photo-3094799.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
-                         <span className="name">Aditya</span>
-                         <button>Logout</button>
+                          <img src={currentUser.photoURL} alt=""/>
+                         <span className={currentUser.displayName}>Aditya</span>
+                         <button onClick={() => signOut(auth)}>Logout</button>
                         </div>
                 </div>
 
