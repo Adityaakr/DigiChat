b82621f5fa88a16ae9d86732a7ebc1493ad98c67
Akumar <128833380+Adityaakr@users.noreply.github.com>
2024-01-02 14:06:40 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link} from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
+
+
 
 const Login = () => {
+
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+  
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+  
+
+//this is for login page ok so when user will enter correct user and password he/she will be loged in
+// when user is registered he needs to login for chat ok
+
+    try {
+      //authenticating user email and password on login page
+     await signInWithEmailAndPassword(auth, email, password)
+     navigate("/");
+    } catch (error) {
+      setErr(true);
+    }
+  };
+
         return (
           <div className="FormContainer">
               <div className="FormWrapper">
               <span className="logo">DigiChat</span>
               <span className="title">Login</span>
-      
-      
-              <form>
-              <input placeholder = "email"/>
-              <input placeholder = "Password"/>
+              <form onSubmit={handleSubmit}>
+              <input type="email" placeholder = "email"/>
+              <input type="password" placeholder = "Password"/>
               <button>Sign in</button>
+              {err && <span>Oops! Something went Wrong!</span>}
               </form>
-              <p>You don't have an Account? Register</p>
+              <p>You don't have an Account? <u><Link to="/register">Register</Link></u></p>
               
               </div>
           </div>
